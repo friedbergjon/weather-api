14e52db4af31adb118a3ae30b98c3fa97c5c693f
friedbergjon <friedberg_jon@hotmail.com>
2019-05-19 20:51:50 -0400

    initial form layout- input field and button

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 import "./style.css";
-
+import axiod from 'axios
+'
 class App extends Component {
   render() {
-    return <div className="App">Hello World</div>;
+    return (
+      <div className="App">
+        <form>
+          <input type="text" name="city" value="" placeholder="city" id="city"/>
+  	      <input type="submit" value="Get Weather" id="getTemp"/>
+        </form>
+      </div>
+    )
   }
-}
 
-export default App;
+export default App; 
+
+//with help from Linda Chin's code//

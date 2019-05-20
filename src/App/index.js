2bb248a0a622b2e3fc265ad20bfd3579260da74b
friedbergjon <friedberg_jon@hotmail.com>
2019-05-19 21:56:06 -0400

    added zipcode functionality

--- src/App/index.js
+++ src/App/index.js
@@ -7,7 +7,7 @@ class App extends Component {
     super(props);
 
     this.state = {
-      city: ""
+     zipcode: ""
     }
 
     this.fetchWeather = this.fetchWeather.bind(this);
@@ -22,22 +22,23 @@ class App extends Component {
 
   async fetchWeather(event) {
     event.preventDefault();
-    const inputCity = this.state.city;
-    const url = "http://api.openweathermap.org/data/2.5/weather?q=";
+    const inputZipcode = this.state.zipcode;
+    const url = "http://api.openweathermap.org/data/2.5/weather?zip=";
     const api_key = "0073d067c1ec39cd7867800a9600025e";
-    const res = await axios(`${url}${inputCity}&APPID=${api_key}`);
+    const res = await axios(`${url}${inputZipcode},us&APPID=${api_key}`);
     console.log(res.data);
   }
 
   render() {
-
-
+//https://openweathermap.org/current
+// URL listed for getting zipcode//
+//help from Andy Jeong code//
 
     
     return (
       <div className="App">
        <form onSubmit={this.fetchWeather}>
-          <input type="text" name="city" value={this.state.city} placeholder="city" id="city" onChange={this.handleInputChange}/>
+          <input type="text" name="zipcode" value={this.state.zipcode} placeholder="zipcode" id="zipcode" onChange={this.handleInputChange}/>
   	      <input type="submit" value="Get Weather" id="getTemp"/>
         </form>
       </div>
